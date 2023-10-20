b6274cdaef3dbedfd789343277f589399694fe22
nkartikaxioned <kartikn@axioned.com>
2023-10-20 19:36:07 +0530

    js20%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,33 @@
-/* Author: 
 
-*/
+var js = $.noConflict();
+js(document).ready(function () {
+  dataload()
+})
+
+const dataload = async () => {
+  try {
+    var result = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!result) {
+      throw new Error('Failed to fetch data');
+    } else {
+      var data = await result.json();
+      console.log(data);
+    }
+  } catch (error) {
+    alert(error);
+  }
+
+  createStructure(data);
+}
 
 
-
-
-
+function createStructure(data) {
+  const dataLength = data.length;
+  let incrementalNumber = 3
+  if (incrementalNumber < dataLength) {
+     
+  }
+}
 
 
 
